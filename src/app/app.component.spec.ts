56d8c50ea7937fee039f7a92228568c18a5d9a9b
Ross Ragsdale <ross.ragsdale@gmail.com>
2018-01-23 22:36:37 -0600

    Fix some more tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [MessageService],
       declarations: [
-        AppComponent
+        AppComponent,
+        MessagesComponent
       ],
     }).compileComponents();
   }));
@@ -13,10 +21,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Tour of Heroes'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
